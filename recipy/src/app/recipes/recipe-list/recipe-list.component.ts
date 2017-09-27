218438ddcf56789febd8afc9ee5fbf906ac83510
dhanisthlm <dhanisthlm@gmail.com>
2017-09-27 21:49:47 +0200

    started working with header and recipe component

--- recipy/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipy/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipies: Recipe[] = [
+    new Recipe('pankakor', 'recept för pannkakor', 'http://www.pancaces.com')
+  ];
 
   constructor() { }
 
